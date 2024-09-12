64c1f0053eff59d262e53010f47c0f00ca38f736
MiyaSHs <124814309+MiyaSHs@users.noreply.github.com>
2024-09-12 16:11:26 -0300

    Update bootloader.nix

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -5,11 +5,5 @@
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.plymouth.enable = true;
-  boot.kernel.sysctl."vm.max_map_count" = 2147483642;
-  boot.kernelPackages = pkgs.linuxPackages_cachyos;
-  chaotic.scx = {
-    enable = true;
-    scheduler = "scx_lavd";
-  };
+  boot.kernelPackages = pkgs.linuxPackages;
 }
