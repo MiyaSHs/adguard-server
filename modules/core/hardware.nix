73b20b88f14d3d2933f841d1e978dc7d1002dc1d
MiyaSHs <124814309+MiyaSHs@users.noreply.github.com>
2024-09-12 16:13:48 -0300

    Update hardware.nix

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -6,18 +6,7 @@
       enable32Bit = true;
     };
     enableRedistributableFirmware = true;
-    amdgpu.initrd.enable = true;
   };
   services.system76-scheduler.enable = true;
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;
-  hardware.cpu.amd.updateMicrocode = true;
-  programs.corectrl = {
-    enable = true;
-    gpuOverclock = {
-      enable = true;
-      ppfeaturemask = "0xffffffff";
-    };
-  };
+  hardware.cpu.intel.updateMicrocode = true;
 }
